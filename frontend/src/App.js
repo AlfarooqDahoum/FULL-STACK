72a51621a9ce51f579f660551ecc40dd748e5bdd
AlfarooqDahoum <113837413+AlfarooqDahoum@users.noreply.github.com>
2023-12-06 12:05:23 +0000

    u

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import Home from './Home'
+import About from './About'
+import Nav from './components/Nav'
 
-function App() {
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+const App = () => {
   return (
     <div>
-
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route 
+        path='/'
+        element={<Home />}
+        />
+        <Route 
+        path='/about'
+        element={<About />}
+        />
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
